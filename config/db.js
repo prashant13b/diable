60d2614a6001b8adac7532a52a80538e88c4efbf
Prashant Bhardwaj <prashantdrew@gmail.com>
2017-12-10 12:46:46 +0530

    added config

---
+++ config/db.js
@@ -0,0 +1,5 @@
+const config = {
+    db: `mongodb+srv://prashant13b:GArBAzxdVYOsWRlu@cluster0-wcsht.mongodb.net/shatpat`
+}
+
+module.exports = config;
---
+++ config/index.js
@@ -0,0 +1,4 @@
+const config={
+    port: process.env.port || 8080
+}
+module.exports = config
