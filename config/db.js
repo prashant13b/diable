f20dc9ecf8ed1a0977bbecf1fa9c2af69c6dcc9d
Prashant Bhardwaj <prashantdrew@gmail.com>
2017-12-10 13:29:44 +0530

    changed address

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 const config = {
-    db: `mongodb+srv://prashant13b:GArBAzxdVYOsWRlu@cluster0-wcsht.mongodb.net/shatpat`
+    db: `mongodb://prashant13b:GArBAzxdVYOsWRlu@cluster0-shard-00-00-wcsht.mongodb.net:27017,cluster0-shard-00-01-wcsht.mongodb.net:27017,cluster0-shard-00-02-wcsht.mongodb.net:27017/shatpat?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin`
 }
 
 module.exports = config;
